4c617c8e5e610e6ce86c3203c47875cbf50fce7d
sumit1092 <sumitnandkar21@gmail.com>
2023-10-16 00:44:08 +0530

    commit

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -13,7 +13,7 @@ export class Navbar extends Component {
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-center">
                 <li className="nav-item">
                   <Link className="nav-link" aria-current="page" to="/">Home</Link>
                </li>
